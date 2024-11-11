68f553343b704d1ceb0b48dd27fe4d5d0be6f72d
Vasya773 <152695790+Vasya773@users.noreply.github.com>
2024-11-11 23:23:21 +0400

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 
 @app.on_event("startup")
-async def shutdown():
+async def startup():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
