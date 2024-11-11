1e4faf09a90b86c38f7b3d77863ff7f99da09b59
Vasya773 <152695790+Vasya773@users.noreply.github.com>
2024-11-11 16:48:20 +0400

    Create test_recipes.py

---
+++ tests/test_recipes.py
@@ -0,0 +1,71 @@
+import pytest
+from .conftest import db_session
+
+
+@pytest.mark.asyncio
+async def test_get_all_recipes(client, db_session):
+    response = client.get('/recipes/')
+    print(response.json())
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
+
+@pytest.mark.asyncio
+async def test_create_new_recipe(client, db_session):
+    new_recipe_data = {
+        'name': 'Пельмени',
+        'number_views': 0,
+        'cooking_time': 20
+    }
+    response = client.post('/recipes', json=new_recipe_data)
+    print(response.json())
+    assert response.status_code == 200
+    assert response.json()['name'] == 'Пельмени'
+
+
+@pytest.mark.asyncio
+async def test_get_recipe_by_id(client, db_session):
+    response = client.get('/recipes/1')
+    print(response.json())
+    assert response.status_code == 200
+    assert response.json()['name'] == 'Пельмени'
+
+
+@pytest.mark.asyncio
+async def test_create_existing_recipe(client, db_session):
+    existing_recipe_data = {
+        'name': 'Пельмени',
+        'number_views': 0,
+        'cooking_time': 20
+    }
+    response = client.post('/recipes', json=existing_recipe_data)
+    print(response.json())
+    assert response.status_code == 400
+    assert response.json()['detail'] == 'Этот рецепт уже существует.'
+
+
+@pytest.mark.asyncio
+async def test_add_ingredients(client, db_session):
+    ingredients_data = {
+        'name': 'Пельмени',
+        'ingredients': 'Тесто, фарш',
+        'description': 'Пельмени домашние'
+    }
+    response = client.post('/deep_recipes?recipe_id=1', json=ingredients_data)
+    print(response.json())
+    assert response.status_code == 200
+    assert response.json()['name'] == 'Пельмени'
+    assert response.json()['ingredients'] == 'Тесто, фарш'
+
+
+@pytest.mark.asyncio
+async def test_add_existing_deep_recipe(client, db_session):
+    ingredients_data = {
+        'name': 'Пельмени',
+        'ingredients': 'Тесто, фарш',
+        'description': 'Пельмени домашние'
+    }
+    response = client.post('/deep_recipes?recipe_id=1', json=ingredients_data)
+    print(response.json())
+    assert response.status_code == 400
+    assert response.json()['detail'] == 'Этот рецепт уже существует.'
