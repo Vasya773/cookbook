0e887899bdf429cd7d7a35e86dc4630a6045cfe5
Vasya773 <152695790+Vasya773@users.noreply.github.com>
2024-11-11 23:37:07 +0400

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ async def get_all_recipes() -> List[Recipes]:
 async def get_recipe_by_id(recipe_id: int):
     """Получение детальной информации о конкретном рецепте по ID"""
     async with async_session() as session:
-        result = await session.execute(select(Recipes).filter(Recipes.recipe_id == recipe_id))
+        result = await session.execute(select(Recipes).where(Recipes.recipe_id == recipe_id))
         recipe = result.scalar_one_or_none()
 
         if recipe is None:
@@ -54,7 +54,7 @@ async def create_new_recipe(recipes: RecipesIn):
         new_recipe = Recipes(**recipes.dict())
 
         existing_recipe = await session.execute(
-            select(Recipes).filter(
+            select(Recipes).where(
                 Recipes.name == recipes.name
             )
         )
@@ -73,7 +73,7 @@ async def create_new_recipe(recipes: RecipesIn):
 async def add_ingredients(recipe_id: int, deep_recipe: DeepRecipesIn):
     """Добавление ингредиентов для создания блюда"""
     async with async_session() as session:
-        result = await session.execute(select(Recipes).filter(Recipes.recipe_id == recipe_id))
+        result = await session.execute(select(Recipes).where(Recipes.recipe_id == recipe_id))
         recipe = result.scalar_one_or_none()
 
         if recipe is None:
